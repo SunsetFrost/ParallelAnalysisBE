1fe281d6a408cf14f0b459f4cfd55a8fb2271fce
SunsetFrost <1262457596@qq.com>
2018-11-29 10:28:29 +0800

    feat: script to add local data to hadoop

--- src/tool/hadoop_operation.py
+++ src/tool/hadoop_operation.py
@@ -1,6 +1,22 @@
 from hdfs import *
-client = Client("http://172.21.213.170:50070", root="/", session=False)
-# print(client.list("/", status=True))
-# client.makedirs("/tmp/upload")
-client.upload("tmp/upload", "D:\\Plan.txt")
-print(client.list("/tmp/upload"))
+
+def mkdir(client, path):
+    client.makedirs(path)
+
+def upload(client, start, end):
+    srcPath = '/home/bowen/Parallel/data/IBIS/standard/sites'
+    targetPath = '/site/ibis/file/sites'
+    for i in range(start, end + 1):
+        filename = str(i) + '_proced.csv'
+        client.upload(targetPath + '/' + filename, srcPath + '/' + filename)
+
+def createSeqFile(client, start, end):
+    # hdfs package not support
+
+def main():
+    client = Client("http://172.21.212.122:50070", root="/", session=False)
+    upload(client, 1, 10000)
+    print(client.list("/site/ibis/file/sites"))
+
+if __name__ == "__main__":
+    main()
